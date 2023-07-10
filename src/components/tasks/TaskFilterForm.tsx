7599a5c90b5d3ad7a4f1edbcea80cc6a876d9073
Sachet Moktan <sachet.moktan@infodevelopers.local>
2023-07-10 11:01:09 +0545

    filter bug fixed

--- src/components/tasks/TaskFilterForm.tsx
+++ src/components/tasks/TaskFilterForm.tsx
@@ -11,95 +11,107 @@ interface IProps {
   setFormData: (data: ITaskFilter) => void;
 }
 
-const TaskFilterForm = ({formData, setFormData} : IProps) => {
+const TaskFilterForm = ({ formData, setFormData }: IProps) => {
 
   const { tasksList, setFilteredTasksList
   } = useContext(TasksContext);
 
-  function handleChange (value: number | string | boolean | Date, name: string) {
+  function handleChange(value: number | string | boolean | Date, name: string) {
     setFormData({ ...formData, [name]: value });
   }
 
-  function handleTaskFilterSubmit () {
+  function handleTaskFilterSubmit() {
     let filteredTL: Array<ITask> = [];
-    if(formData?.dueDate || formData?.status) {
-    if (formData?.dueDate) {
-      const filteredByDD = tasksList.filter((item) => {
-        if(moment(item.dueDate).format('YYYY-MM-DD') == moment(formData.dueDate).format('YYYY-MM-DD'))
-        return item
+
+    if (formData?.dueDate && formData?.status) {
+      const filteredByBoth = tasksList.filter((item) => {
+        if ((moment(item.dueDate).format('YYYY-MM-DD') == moment(formData.dueDate).format('YYYY-MM-DD')) &&
+          (item.status && formData.status && item.status.value == formData.status.value)
+        )
+          return item
       })
-      filteredTL = [...filteredTL, ...filteredByDD]
-    } if (formData?.status) {
-      const filteredByStatus = tasksList.filter((item) => {
-        if(item.status && formData.status && item.status.value == formData.status.value)
-            return item 
-      })
-      filteredTL = [...filteredTL, ...filteredByStatus]
+      setFilteredTasksList(filteredByBoth);
+      return;
     }
-    setFilteredTasksList(filteredTL);
-    } 
+
+    if (formData?.dueDate || formData?.status) {
+      if (formData?.dueDate) {
+        const filteredByDD = tasksList.filter((item) => {
+          if (moment(item.dueDate).format('YYYY-MM-DD') == moment(formData.dueDate).format('YYYY-MM-DD'))
+            return item
+        })
+        filteredTL = [...filteredTL, ...filteredByDD]
+      } if (formData?.status) {
+        const filteredByStatus = tasksList.filter((item) => {
+          if (item.status && formData.status && item.status.value == formData.status.value)
+            return item
+        })
+        filteredTL = [...filteredTL, ...filteredByStatus]
+      }
+      setFilteredTasksList(filteredTL);
+    }
     else {
       setFilteredTasksList(tasksList)
     }
   }
 
-  function handleResetFilter () {
+  function handleResetFilter() {
     setFilteredTasksList(tasksList)
     setFormData(initialTaskFilter)
   }
 
   return (
     <form className="h-100 p-3" onSubmit={(e) => {
-      e.preventDefault(); 
+      e.preventDefault();
       handleTaskFilterSubmit();
-      }}>
-        <div className="d-flex justify-content-around align-items-center">
-            <div>
-                <label htmlFor='dueDate'>Due Date</label>
-                <div>
-                <ReactDatePicker
-                placeholderText='Select Due Date...' 
-                className="form-control form-control-sm" 
-                selected={formData?.dueDate} 
-                onChange={(event: Date) => {handleChange(event, 'dueDate')}} 
-                />
-                </div>
-            </div>
-
-            <div>
-                <label htmlFor='description'>Task Status</label>
-                <Select
-                name="status"
-                options={TASK_OPT}
-                className="basic-multi-select"
-                classNamePrefix="select"
-                menuPosition="fixed"
-                value={formData?.status}
-                onChange={(event: any) => {
-                    handleChange(event, "status")
-                }}
-                isLoading={false}
-                isMulti={false}
-                />
-            </div>
-
+    }}>
+      <div className="d-flex justify-content-around align-items-center">
+        <div>
+          <label htmlFor='dueDate'>Due Date</label>
           <div>
-            <button 
-            className='btn btn-filter text-white me-2' 
+            <ReactDatePicker
+              placeholderText='Select Due Date...'
+              className="form-control form-control-sm"
+              selected={formData?.dueDate}
+              onChange={(event: Date) => { handleChange(event, 'dueDate') }}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor='description'>Task Status</label>
+          <Select
+            name="status"
+            options={TASK_OPT}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            menuPosition="fixed"
+            value={formData?.status}
+            onChange={(event: any) => {
+              handleChange(event, "status")
+            }}
+            isLoading={false}
+            isMulti={false}
+          />
+        </div>
+
+        <div>
+          <button
+            className='btn btn-filter text-white me-2'
             type='submit'
-            >
+          >
             Filter
-            </button>
-            <button 
-            className='btn btn-secondary text-white' 
+          </button>
+          <button
+            className='btn btn-secondary text-white'
             type='button'
             onClick={() => handleResetFilter()}
-            >
+          >
             Reset
-            </button>
-          </div>
-
+          </button>
         </div>
+
+      </div>
     </form>
   )
 }
